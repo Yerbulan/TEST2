fdbb615eabf65da97a479b719f461e452b7aaafd
Yerbulan <bula136@gmail.com>
2022-09-07 16:06:27 +0600

    Trying Committing in PyCharm

---
+++ extract_xml.py
@@ -0,0 +1,15 @@
+import xml.etree.ElementTree as ET
+import urllib.request
+
+url = 'http://py4e-data.dr-chuck.net/comments_1547489.xml'
+response = urllib.request.urlopen(url).read()
+tree = ET.fromstring(response)
+
+r = 0
+
+for i in tree:
+    for n in i:
+        f = int(n.find('count').text)
+        r += f
+
+print(r)
